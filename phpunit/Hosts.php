bc3c3b90bd5d8dfb0ccaac7bb343255ae38ff741
Brad Daily <brad@slideshowpro.net>
2013-12-02 15:41:31 -0600

    Rackspace test, another unknown check

--- phpunit/Hosts.php
+++ phpunit/Hosts.php
@@ -7,6 +7,7 @@ class Hosts extends PHPUnit_Framework_TestCase
 	public function testUnknown()
 	{
 		$host = new WebhostWhois;
+		$this->assertFalse($host->isBluehost());
 		$this->assertEquals($host->key, 'unknown');
 	}
 
@@ -57,6 +58,13 @@ class Hosts extends PHPUnit_Framework_TestCase
 		$host = new WebhostWhois('Linux webm215.90.ha.ovh.net 3.10.11-mutu-grs-ipv6-64 #1 SMP Tue Sep 24 18:07:54 CEST 2013 x86_64');
 		$this->assertTrue($host->isOvh());
 		$this->assertEquals($host->key, 'ovh');
+	}
+
+	public function testRackspaceCloud()
+	{
+		$host = new WebhostWhois('Linux php53-v08.wc1.ord1.stabletransit.com 2.6.32.39-grsec-3.mosso5.1.x86_64 #1 SMP Mon May 16 20:37:28 CDT 2011 x86_64');
+		$this->assertTrue($host->isRackspaceCloud());
+		$this->assertEquals($host->key, 'rackspace-cloud');
 	}
 
 	public function testSite5()
